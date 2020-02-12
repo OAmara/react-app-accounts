23cc946f99650d1cfab6969e0d19850c4223eab9
Omar Amara <O_amara@hotmail.com>
2020-02-12 13:17:48 -0600

    created AccountContainer Component to fetch accounts from API

---
+++ react-accounts/src/AccountContainer/index.js
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+
+class AccountContainer extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			accounts: []
+		}
+	}
+
+	render() {
+		return(
+			'AccountContainer'
+		)
+	}
+}
+
+export default AccountContainer
--- react-accounts/src/App.js
+++ react-accounts/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import AccountContainer from './AccountContainer'
 
 function App() {
+
   return (
     <div className="App">
-
+      <AccountContainer />
     </div>
   );
 }
