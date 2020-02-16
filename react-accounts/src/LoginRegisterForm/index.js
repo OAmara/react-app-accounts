81de223a4a63291b407a208f22c47d9b8355740f
Omar Amara <O_amara@hotmail.com>
2020-02-16 16:37:54 -0600

    login tracked in state, form switches between login/register

--- react-accounts/src/LoginRegisterForm/index.js
+++ react-accounts/src/LoginRegisterForm/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Button, Label } from 'semantic-ui-react'
+import './index.css'
 
 class LoginRegisterForm extends Component {
 	constructor(props) {
@@ -9,19 +10,37 @@ class LoginRegisterForm extends Component {
 			username: '',
 			email: '',
 			password: '',
+			action: 'login' // login or register
 		}
 	}
+
+	switchForm = () => {
+		this.setState({
+			action: this.state.action === 'register' ? 'login' : 'register'
+		})
+	}
+
 	render() {
 		return(
 			<div className="LoginRegisterForm">
+				<h2 className='LoginRegisterForm-h2'>{this.state.action + ' here'}</h2>
 				<Form>
-					<Label>Username:</Label>
-					<Form.Input
-						type='text'
-						name='username'
-						placeholder='Enter Username'
-						value={this.state.username}
-					/>
+					{
+						// username should only show when registering
+						this.state.action === 'register'
+						?
+						<React.Fragment>
+							<Label>Username:</Label>
+							<Form.Input
+								type='text'
+								name='username'
+								placeholder='Enter Username'
+								value={this.state.username}
+							/>
+						</React.Fragment>
+						:
+						null
+					}
 					<Label>Email:</Label>
 					<Form.Input
 						type='text'
@@ -36,8 +55,17 @@ class LoginRegisterForm extends Component {
 						placeholder='Enter Password'
 						value={this.state.password}
 					/>
-					<Button type='Submit'>Log In</Button>
+					<Button type='Submit'>{this.state.action === 'register' ? 'Register' : 'Login'}</Button>
 				</Form>
+				{
+					this.state.action === 'register'
+					?
+					// register page
+					<small>Already have an account? Log in <span className='fake-link' onClick={this.switchForm}>here</span>.</small>
+					:
+					// login page
+					<small>Need an account? Sign up <span className='fake-link' onClick={this.switchForm}>here</span>!</small>
+				}
 			</div>
 		)
 	}
